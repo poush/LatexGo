708332fe08008339c89130078dd0a0f4e002755a
Bhavya <bhavya18333@iiitd.ac.in>
2020-04-06 22:21:11 +0530

    shift firebase config parameters to config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// firebase config parameters
+
+var config = {
+    apiKey: "AIzaSyA5n3j29EO1AyQwi3huuHDUf3Fdvo24oOc",
+    authDomain: "xyz-jmfdxq.firebaseapp.com",
+    databaseURL: "https://xyz-jmfdxq.firebaseio.com",
+    projectId: "xyz-jmfdxq",
+    storageBucket: "xyz-jmfdxq.appspot.com",
+    messagingSenderId: "505581208144"
+  };

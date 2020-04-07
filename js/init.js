f38b1da659a718d83ec858c8ac062eda236fb7ef
Bhavya Chopra <43749512+BhavyaC16@users.noreply.github.com>
2020-04-07 14:40:20 +0530

    Shift firebase config parameters to config file (#15)
    * shift firebase config parameters to config.js
    
    * loading config before init

--- js/init.js
+++ js/init.js
@@ -3,14 +3,6 @@ var session;
 
 function init() {
 
-  var config = {
-    apiKey: "AIzaSyA5n3j29EO1AyQwi3huuHDUf3Fdvo24oOc",
-    authDomain: "xyz-jmfdxq.firebaseapp.com",
-    databaseURL: "https://xyz-jmfdxq.firebaseio.com",
-    projectId: "xyz-jmfdxq",
-    storageBucket: "xyz-jmfdxq.appspot.com",
-    messagingSenderId: "505581208144"
-  };
   firebase.initializeApp(config);
 
 

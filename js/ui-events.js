0ed27f83a6c32f25eb6f71414f2bdeaeee5793b5
escher-m <62093616+escher-m@users.noreply.github.com>
2020-04-29 17:02:52 +0530

    Create ui-events.js

---
+++ js/ui-events.js
@@ -0,0 +1,13 @@
+var footercb = document.getElementById("compile");
+
+var event = new Event('spinner');
+
+// Listen for the event.
+footercb.addEventListener('spinner', function(compile) {
+    setInterval(function () {
+        .loader {
+          border-top: 16px solid blue;
+          border-bottom: 16px solid blue;
+        }
+    });
+});
